fe0d9254d460d40bed26ee0d9117485d7260601d
1 <a1@1s-MacBook-Pro.local>
2023-08-13 17:12:13 +0530

    added blog functionality

--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,13 +1,23 @@
 import React from 'react'
 import  {useRouter } from 'next/router';
+import Styles from '@/styles/blogpost.module.css'
  
 
 const slug = () => {
     const router=useRouter();
+    const {slug}=router.query
     console.log(router.query)
   return (
-
-    <div>Dynamic route</div>
+    <div className={Styles.container}>
+      <h1>Title of the page {slug}</h1>
+      <hr />
+      <div className={Styles.main}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptates distinctio porro aperiam deleniti. Quia nam deleniti amet. Maiores impedit, vitae labore reprehenderit ipsa earum inventore minus alias adipisci excepturi!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam consequuntur alias enim non expedita iusto totam iure eaque aut ducimus esse omnis, dignissimos, accusamus quo quasi sit placeat laboriosam. Unde!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quas et deleniti minima unde eaque quae obcaecati facere, fugit architecto rem! Consequatur voluptates autem dolorum minus nemo. Cupiditate, quos libero.
+      </div>
+    </div>
+    
   )
 }
 
--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import styles from '@/styles/blogs.module.css'
+import Link from 'next/link'
 
 const blogs = () => {
   return (
-    <div>this is my blogs component</div>
+    <main className={styles.main}>
+    <div className="blogs">
+          <div className="blog-item">
+            <Link href={'/blogpost/learn-javascript'}>
+            <h1>how to learn javascript?</h1></Link>
+            <p>javascript is a language used for logic backend in web</p>
+          </div>
+          <div className="blog-item">
+          <Link href={'/blogpost/learn-react-native'}>
+            
+            <h1>how to learn react-native?</h1></Link>
+            <p>javascript is a language used for logic backend in web</p>
+          </div>
+          <div className="blog-item">
+
+          <Link href={'/blogpost/learn-next.js'}>
+            
+            <h1>how to learn next.js</h1></Link>
+            <p>javascript is a language used for logic backend in web</p>
+          </div>
+        </div>
+        </main>
   )
 }
 
